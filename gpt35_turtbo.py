68863379d7d411177c8fb2273a946412be493d39
Vu-Ngoc-Truong <truong.vungoc@meiko.vn>
2023-03-11 16:49:48 +0700

    use gpt 3.5 turbo API

--- gpt35_turtbo.py
+++ gpt35_turtbo.py
@@ -1,28 +1,160 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
 
-
-api_key = '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'
-import openai
+from text_to_speech import text_to_speech
+from speech_to_text import ListenAudio
+import json
+import yaml
 import os
+import time
+import openai
+
+# API Key
+api_key = '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'
 openai.api_key = api_key
-# openai.organization = "MEIKO"
+# organization of MEIKO
+openai.organization = "org-ORyxAR79nusDu86NyNXgXtC4"
 
-conversation=[{"role": "system", "content": "You are a helpful assistant."}]
+class Chatbot:
+    def __init__(self, language='vi', use_speaker=True, use_ROS=False, max_tokens_ans=300):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.language = language
+        self.use_speaker = use_speaker
+        self.use_ROS = use_ROS
+        self.led_effect = "INIT"
+        self.listen_audio = ListenAudio(True)
+        if self.use_ROS:
+            import rospy
+            from std_msgs.msg import String
+            self.pub = rospy.Publisher('chatbot_status', String, queue_size=10)
+            rospy.init_node('chatbot_talk')
+            self.pub.publish(self.led_effect)
 
-while(True):
-    user_input = input("You: ")
-    if user_input == "bye":
-        break
-    conversation.append({"role": "user", "content": user_input})
+        # Number of user and assistant message is saved in conversation, if =0 is save all message
+        self.history_length = 1
+        self.max_tokens_ans = max_tokens_ans
+        if self.language == "vi":
+            self.short_msg = ".Trả lời ngắn dưới " + str(self.max_tokens_ans) + " tokens"
+        if self.language == "ja":
+            self.short_msg = "。 " + str(self.max_tokens_ans) + " トークン未満の短い回答。"
+        if self.language == "en":
+            self.short_msg = ".Short answer under " + str(self.max_tokens_ans) + " tokens."
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages = conversation,
-        temperature=2,
-        max_tokens=1024,
-        top_p=0.9
-    )
-    for choice in response['choices']:
-        print("\n" + choice['message']['content'] + "\n")
+        # Set the behavior of the assistant, instructed with "You are a helpful assistant."
+        self.conversation=[{"role": "system", "content": "You are a helpful assistant.Short answer under 300 tokens."}]
 
-    conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-    print("\n" + response['choices'][0]['message']['content'] + "\n")
+        with open(self.dir_path + '/train/train_dict.json') as json_file:
+            self.data_dict = json.load(json_file)
+
+        with open(self.dir_path + '/train/keywords_default.json') as json_file:
+            self.keywords_dict = json.load(json_file)
+
+    def run(self):
+        try:
+            #####  Get input ask  ########
+            # input_text = input("Mời bạn hỏi: ")
+            input_text = self.listen_audio.listen( self.language).lower()
+            if input_text == "Keyboard Interrupted":
+                return "Keyboard Interrupted"
+
+            if (input_text == ''):
+                print("input text is None")
+                time.sleep(0.1)
+                return "None"
+
+            if (u"goodbye" in input_text) or (u"cảm ơn" in input_text) or (u"ありがとう" in input_text):
+                if self.use_speaker:
+                    if self.language == "vi":
+                        text_to_speech("Tạm biệt quý khách, hẹn gặp lại quý khách.", self.language)
+                    if self.language == "ja":
+                        text_to_speech("さようならお客様、またお会いしましょう。", self.language)
+                    if self.language == "en":
+                        text_to_speech("Goodbye and see you again soon.", self.language)
+                return "Done"
+
+            # Kiểm tra từ khóa đặc biệt trong câu hỏi hay không
+            have_keywords = False
+            keyword_finded = ""
+            for keyword_str in self.keywords_dict:
+                if keyword_str in input_text:
+                    have_keywords = True
+                    # nếu có nhiều keyword cùng xuất hiện thì lấy keyword dài nhất
+                    if len(keyword_str) > len(keyword_finded):
+                        keyword_finded = keyword_str
+
+            # nếu chatbot mkac không trả lời được
+            # nếu có keyword thì trả lời theo mặc định của keyword
+            if have_keywords:
+                response = self.data_dict[str(self.keywords_dict[keyword_finded])]
+                print("ChatBot:#", response)
+                if self.use_speaker:
+                    text_to_speech(response)
+            else:
+                # nếu không có keyword thì hỏi chatGPT
+                # # loại trừ những câu hỏi quá ngắn có thể do âm thanh nhiễu
+                # if len(input_text) < 3:
+                #     print("input text is short: ", len(input_text))
+                #     return "Short"
+                # hỏi chatgpt với các câu hỏi đủ dài
+                print("ChatGPT:>>")
+                self.conversation.append({"role": "user", "content": (input_text + self.short_msg)})
+
+                # Creat chatbot GPT 3.5 turbo
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo-0301",
+                    messages = self.conversation,
+                    temperature=1.0,
+                    max_tokens=self.max_tokens_ans+10,
+                    top_p=0.9
+                )
+                # for choice in response['choices']:
+                #     print("\n" + choice['message']['content'] + "\n")
+
+                # add assistant messages to conversation
+                self.conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+                # Cut history
+                while ( self.history_length > 0) and (len(self.conversation) > (2*self.history_length +1)) or (len(self.conversation) > 7):
+                    # Remove user message
+                    self.conversation.pop(1)
+                    # Remove assistant message
+                    self.conversation.pop(2)
+                    # print("Cut history, conversison lenth is ", len(self.conversation))
+                answer = response['choices'][0]['message']['content']
+                # print(response)
+                if self.use_ROS:
+                    self.led_effect = "DONE"
+                    self.pub.publish(self.led_effect)
+                print(response['usage'])
+                print("GPT-3.5: ", answer)
+
+        except KeyboardInterrupt:
+            print ('Keyboard Interrupted1')
+            return "Keyboard Interrupted"
+
+        except:
+            print("chatbot error!!!")
+            if self.use_ROS:
+                self.led_effect = "ERROR"
+                self.pub.publish(self.led_effect)
+            if self.use_speaker:
+                if self.language == "vi":
+                    text_to_speech("Xin lỗi, tôi đã gặp sự cố khi tìm câu trả lời.", self.language)
+                if self.language == "ja":
+                    text_to_speech("申し訳ありませんが、答えを見つけるのに苦労しました。", self.language)
+                if self.language == "en":
+                    text_to_speech("Sorry, I had trouble finding the answer.", self.language)
+            return "Error"
+
+
+if __name__ == '__main__':
+
+    chatbot_mkac = Chatbot(use_speaker=False,use_ROS=True)
+    while(True):
+        try:
+            result = chatbot_mkac.run()
+            if result == "Keyboard Interrupted" or result == "Done":
+                break
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            print("keyboard")
+            break
--- speech_to_text.py
+++ speech_to_text.py
@@ -5,12 +5,85 @@ import os
 import sys
 import speech_recognition as sr
 import time
-import audioop
 import playsound
 HOME = os.path.expanduser('~')
+class ListenAudio:
+    def __init__(self, use_ROS=False, auto_language=False):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.use_ROS = use_ROS
+        languages_list = ["vi", "en", "ja"]
 
-def listen_audio(language='vi', auto_language=False):
+        if self.use_ROS:
+            import rospy
+            from std_msgs.msg import String
+            self.pub = rospy.Publisher('chatbot_status', String, queue_size=10)
+        # print(dir_path)
+        self.c = sr.Recognizer() # Khởi tạo biến nhận dạng giọng nói
+        self.energy_threshold = 1000  # Ngưỡng năng lượng để xác định có lấy âm hay không.
+        self.pause_threshold = 1.5 # Thời gian xác nhận đã dừng nói để kết thúc nghe.
+        self.dynamic_energy_threshold = True  # Tự động xác định ngưỡng năng lượng
+        # c.operation_timeout = 20 # Thời gian tối đa (theo giây) mà một hoạt động (ví dụ: yêu cầu API) có thể chạy trước khi hết thời gian chờ. Nếu giá trị được thiết lập là None, thì không có giới hạn thời gian chờ.
+
+    def listen(self, language='vi'):
+        try:
+            with sr.Microphone() as source: # Lấy nguồn nói từ Microphone
+
+                self.c.adjust_for_ambient_noise(source, duration= 2.0)
+                # for count in range(5):
+                #     c.adjust_for_ambient_noise(source, duration= 4.0)
+                #     print("energy_threshold: ", c.energy_threshold)
+                #     count +=1
+                #     if c.energy_threshold < 2000:
+                #         break
+                # if c.energy_threshold > 2000:
+                #     return "None"
+                print("energy_threshold: ", self.c.energy_threshold)
+                # buffer = source.stream.read(source.CHUNK)
+                # energy = audioop.rms(buffer, source.SAMPLE_WIDTH)  # energy of the audio signal
+                # print("energy: ", energy)
+                # seconds_per_buffer = (source.CHUNK + 0.0) / source.SAMPLE_RATE
+                # damping = c.dynamic_energy_adjustment_damping ** seconds_per_buffer  # account for different chunk sizes and rates
+                # print("damping: ", damping )
+                if self.use_ROS:
+                    self.pub.publish("LISTEN")
+                playsound.playsound(self.dir_path+ '/logon.mp3', True)
+                print('Listening...')
+                try:
+                    audio = self.c.listen(source,timeout=10, phrase_time_limit= 20) # Biến audio là giá trị dạng chuỗi sau khi máy nghe và nhận dạng từ nguồn vào
+                    playsound.playsound(self.dir_path+ '/logoff.mp3', True)
+
+
+                    # Recognizing audio file
+                    if self.use_ROS:
+                        self.pub.publish("RECOG")
+                    print("Recognizing")
+                    query = self.c.recognize_google(audio, language=language)
+                    print("Customer: ",query)
+                except sr.WaitTimeoutError:
+                    if self.use_ROS:
+                        self.pub.publish("TIMEOUT")
+                    playsound.playsound(self.dir_path+ '/logoff.mp3', True)
+                    print("WaitTimeoutError")
+                    return ''
+
+            return query # Tra ve text
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+            return ''
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            return ''
+        except KeyboardInterrupt:
+            print ('Keyboard Interrupted')
+            return 'Keyboard Interrupted'
+
+def listen_audio(language='vi', use_ROS=False, auto_language=False):
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    if use_ROS:
+        import rospy
+        from std_msgs.msg import String
+        pub = rospy.Publisher('chatbot_status', String, queue_size=10)
+
     languages_list = ["vi", "en", "ja"]
     # print(dir_path)
     c = sr.Recognizer() # Khởi tạo biến nhận dạng giọng nói
@@ -31,65 +104,72 @@ def listen_audio(language='vi', auto_language=False):
             # if c.energy_threshold > 2000:
             #     return "None"
             print("energy_threshold: ", c.energy_threshold)
-            buffer = source.stream.read(source.CHUNK)
-            energy = audioop.rms(buffer, source.SAMPLE_WIDTH)  # energy of the audio signal
+            # buffer = source.stream.read(source.CHUNK)
+            # energy = audioop.rms(buffer, source.SAMPLE_WIDTH)  # energy of the audio signal
             # print("energy: ", energy)
-            seconds_per_buffer = (source.CHUNK + 0.0) / source.SAMPLE_RATE
-            damping = c.dynamic_energy_adjustment_damping ** seconds_per_buffer  # account for different chunk sizes and rates
-            print("damping: ", damping )
+            # seconds_per_buffer = (source.CHUNK + 0.0) / source.SAMPLE_RATE
+            # damping = c.dynamic_energy_adjustment_damping ** seconds_per_buffer  # account for different chunk sizes and rates
+            # print("damping: ", damping )
+            if use_ROS:
+                pub.publish("LISTEN")
             playsound.playsound(dir_path+ '/logon.mp3', True)
             print('Listening...')
             try:
                 audio = c.listen(source,timeout=10, phrase_time_limit= 20) # Biến audio là giá trị dạng chuỗi sau khi máy nghe và nhận dạng từ nguồn vào
                 playsound.playsound(dir_path+ '/logoff.mp3', True)
-                cropped_audio = audio.get_segment(0, 4000) # cắt lấy 5 giây đầu tiên
+                # cropped_audio = audio.get_segment(0, 4000) # cắt lấy 5 giây đầu tiên
                 # with open("recording.wav", "wb") as f:
                 #     f.write(cropped_audio.get_wav_data())
 
-                # Auto detect language
-                if auto_language:
-                    confidences = []
-                    query_list = []
-                    len_query_list = []
-                    start_time = time.time()
+                # # Auto detect language
+                # if auto_language:
+                #     confidences = []
+                #     query_list = []
+                #     len_query_list = []
+                #     start_time = time.time()
 
-                    for lang in languages_list:
-                        try:
-                            results = c.recognize_google(cropped_audio, language=lang, show_all=True)
-                            # print("query:", results)
-                            if "alternative" in results:
-                                query_ = results['alternative'][0]['transcript']
-                                confidence = results['alternative'][0]['confidence']
+                #     for lang in languages_list:
+                #         try:
+                #             results = c.recognize_google(cropped_audio, language=lang, show_all=True)
+                #             # print("query:", results)
+                #             if "alternative" in results:
+                #                 query_ = results['alternative'][0]['transcript']
+                #                 confidence = results['alternative'][0]['confidence']
 
-                            else:
-                                query_ = ""
-                                confidence = 0
+                #             else:
+                #                 query_ = ""
+                #                 confidence = 0
 
-                        except:
-                            print("Could not understand audio")
-                            query_ = ""
-                            confidence = 0
+                #         except:
+                #             print("Could not understand audio")
+                #             query_ = ""
+                #             confidence = 0
 
-                        confidences.append(confidence)
-                        query_list.append(query_)
-                        len_query_list.append(len(query_))
+                #         confidences.append(confidence)
+                #         query_list.append(query_)
+                #         len_query_list.append(len(query_))
 
-                    elapsed_time = time.time() - start_time
-                    print("Time2:", elapsed_time)
-                    print(query_list)
-                    print(len_query_list)
-                    print(confidences)
-                    # print(lang_result_list)
-                    index_min = len_query_list.index(min(len_query_list))
-                    confidences[index_min] = 0
-                    index = confidences.index(max(confidences))
-                    language = languages_list[index]
-                    print("Language is: ", language)
+                #     elapsed_time = time.time() - start_time
+                #     print("Time2:", elapsed_time)
+                #     print(query_list)
+                #     print(len_query_list)
+                #     print(confidences)
+                #     # print(lang_result_list)
+                #     index_min = len_query_list.index(min(len_query_list))
+                #     confidences[index_min] = 0
+                #     index = confidences.index(max(confidences))
+                #     language = languages_list[index]
+                #     print("Language is: ", language)
+
                 # Recognizing audio file
+                if use_ROS:
+                    pub.publish("RECOG")
                 print("Recognizing")
                 query = c.recognize_google(audio, language=language)
                 print("Customer: ",query)
             except sr.WaitTimeoutError:
+                if use_ROS:
+                    pub.publish("TIMEOUT")
                 playsound.playsound(dir_path+ '/logoff.mp3', True)
                 print("WaitTimeoutError")
                 return ''
@@ -108,10 +188,11 @@ def listen_audio(language='vi', auto_language=False):
 
 def giao_tiep_voi_khach():
 
+    reg_audio = ListenAudio(use_ROS=True)
     while(True):
 
         try:
-            query = listen_audio("ja").lower()
+            query = reg_audio.listen("vi").lower()
             if query == "Keyboard Interrupted" or ("goodbye" in  query):
                 return
             time.sleep(0.1)
